159daf5094543b57a1664112fb58509e025a8287
bpalmer661 <59478417+bpalmer661@users.noreply.github.com>
2020-09-20 14:01:55 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 
-const admin = require('firebase-admin');
+//npm i busboy
+//npm i react
+//npm i react-redux
 
 
-admin.initializeApp();
+
+
 
 const functions = require('firebase-functions');
 
@@ -10,7 +13,26 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 
-const db = admin.firestore();
+
+const { getAllJobPosts,createJobPost,getJobPostAndReplys, replyToJobPost,deleteJobPost,deleteJobPostReply} = require('./handlers/jobPosts')
+
+
+const { signup, login, uploadImage,addUserDetails, getOurJobPosts,getUserDetails, markNotificationsAsRead } = require('./handlers/users')
+
+
+const { fbAuth } = require('./util/fbAuth');
+
+
+const { db } = require('./util/admin')
+
+
+//Job Routes
+app.get('/JobPosts',getAllJobPosts );
+app.post('/createJobPost',fbAuth,createJobPost);
+app.get('/JobPost/:jobPostId',getJobPostAndReplys)
+app.post('/JobPost/:jobPostId',fbAuth,replyToJobPost )
+app.delete('/JobPost/:jobPostId',fbAuth,deleteJobPost)
+app.delete('/deleteJobPostReply/:jobPostReplyId',fbAuth,deleteJobPostReply)
 
 
 
@@ -18,156 +40,14 @@ const db = admin.firestore();
 
 
 
-const firebase = require('firebase')
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBEkSLuI3itUsX3iIhvDnnHHT7WoCT76qI",
-  authDomain: "workservices-e4506.firebaseapp.com",
-  databaseURL: "https://workservices-e4506.firebaseio.com",
-  projectId: "workservices-e4506",
-  storageBucket: "workservices-e4506.appspot.com",
-  messagingSenderId: "232325006209",
-  appId: "1:232325006209:web:da0aca044337ef4b119e80",
-  measurementId: "G-K8BRW9Y8HL"
-};
-
-  firebase.initializeApp(firebaseConfig);
-
-
-
-
-
-
-
-
-
-app.get('/JobPosts', (req, res) => {
-    admin.firestore().collection('JobPosts').orderBy('createdAt','desc').get()
-    .then(data => {
-        let jobPosts = [];
-        data.forEach(doc => {
-
-
-            // jobPosts.push(doc.data())
-
-            jobPosts.push({
-                jobId: doc.id,
-                ...doc.data()
-            });
-
-        });
-    return res.json(jobPosts);
-    })
-    .catch(err => console.error(err));
-    });
-
-
-
-
-app.post('/createJobPost', (req, res) => {
-const newJob = {
-    jobDescription: req.body.jobDescription,
-    jobTitle: req.body.jobTitle,
-    createdAt: new Date().toISOString()
-};
-admin.firestore().collection('JobPosts').add(newJob)
-.then(docRef => {
-res.json({message: `document ${docRef.id} created succesfully`})
-})
-.catch(err => {
-    //500 server error
-    res.status(500).json({error: 'something went wrong'})
-    console.error(err)
-})
-    });
-
-
-
-/////////lesson #7 ////////////////
-
-const isEmail = (email) => {
-    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-}
-
-
-
-    const isEmpty  = (string) => {
-        if(string.trim() === '') return true
-        else return false;
-    }
-    /////////lesson #7 ////////////////
-
-
-
-
-//sign up route
-app.post('/signup', (req, res) => { 
-    const user = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    username:req.body.username
-};
-
-
-/////////lesson #7 ////////////////
-let errors = {};
-
-if(isEmpty(user.email)){
-    errors.email = "Email Must Not Be Empty",
-} else if(){
-
-}
-
-/////////lesson #7 ////////////////
-
-
-//validate data
-let token, userId; 
-admin.firestore().doc(`/users/${user.username}`).get()
-.then((doc) => {
-if(doc.exists){
-    ///bad request
-    return res.status(400).json({
-        username: 'this username is already taken'});
-} else {
-    return firebase.auth().createUserWithEmailAndPassword(user.email,user.password)
-}
-})
-.then((data) =>{
-userId = data.user.uid;
-   return  data.user.getIdToken();
-})
-.then(token =>{
-token = tokenId;
-const userInfo = {
-    username: user.username,
-    email: user.email,
-    createdAt: new Date().toISOString(),
-    userId:  userId,
-};
-admin.firestore().doc(`/users/${user.username}`).set(userInfo)
-}).then(() =>{
-    //201 data created successfully
-    return res.status(201).json({ token });
-})
-.catch((err) => { 
-    console.error(err);
-    if(err.code === "auth/email-already-in-use"){
-        ///400 
-        return res.status(400).json({ email: 'Email is already in use'})
-    } else {
-        return res.status(500).json({error: err.code });
-    }
-    });
-});
-
-
-
-
-
+//user routes 
+app.post('/signup', signup);
+app.post('/login',login);
+app.post('/user/image',fbAuth, uploadImage)
+app.post('/user',fbAuth,addUserDetails)
+app.get('/user',fbAuth,getOurJobPosts)
+app.get('/user/:username',getUserDetails)
+app.post('/notifications',fbAuth,markNotificationsAsRead)
 
 
 
@@ -177,3 +57,118 @@ admin.firestore().doc(`/users/${user.username}`).set(userInfo)
   exports.api = functions.region('australia-southeast1').https.onRequest(app);
 
 
+
+
+
+  exports.createNotificationOnJobPostReply = functions
+  .region('australia-southeast1')
+  .firestore.document('jobReplys/{id}')
+  .onCreate((snapshot) => {
+
+    return db.collection("JobPosts").doc(`${snapshot.data().jobPostId}`)
+      .get()
+      .then((doc) => {
+        console.log("sender and reciever =" + doc.data().username + snapshot.data().username)
+        if (
+          doc.exists &&
+          doc.data().username !== snapshot.data().username
+        ) {
+         
+          return db.collection("notifications").doc(`${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().username,
+            sender: snapshot.data().username,
+            read: false,
+            jobPostId: doc.id
+          });
+        } else {
+          console.log("document does not exists or sender and receiver are the same")
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+
+
+
+
+  exports.deleteJobReplyNotificationOnJobReplyDelete = functions
+  .region('australia-southeast1')
+  .firestore.document('jobReplys/{id}')
+  .onDelete((snapshot) => {
+
+    return db.collection("notifications").doc(`${snapshot.id}`).delete().
+    then(() => {
+return;
+    })
+    .catch((err) => {
+      console.error(err);
+      return;
+    });
+  });
+
+
+
+
+
+
+
+exports.onUserImageChange = functions
+  .region('australia-southeast1')
+  .firestore.document('/users/{userId}')
+  .onUpdate((change) => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if (change.before.data().userImageUrl !== change.after.data().userImageUrl) {
+      console.log('image has changed');
+      const batch = db.batch();
+      return db
+        .collection('JobPosts')
+        .where('username', '==', change.before.data().username)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const JobPosts = db.doc(`/JobPosts/${doc.id}`);
+            batch.update(JobPosts, { userImageUrl: change.after.data().userImageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
+
+
+
+
+
+exports.onJobPostDelete = functions
+.region('australia-southeast1')
+  .firestore.document('/JobPosts/{JobPostId}')
+  .onDelete((snapshot, context) => {
+    const JobPostId = context.params.JobPostId;
+    const batch = db.batch();
+
+    return db
+      .collection('jobReplys')
+      .where('jobPostId', '==', JobPostId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.collection("jobReplys").doc(`${doc.id}`));
+        });
+
+
+        return db
+          .collection('notifications')
+          .where('jobPostId', '==', JobPostId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.collection("notifications").doc(`${doc.id}`));
+        });
+        return batch.commit();
+      })
+
+      
+
+      .catch((err) => console.error(err));
+  });
