e2641b31a2f19113b8cc8dd5e4a5709693b860c8
bpalmer661 <59478417+bpalmer661@users.noreply.github.com>
2020-10-15 19:53:43 +0800

    Add files via upload

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBEkSLuI3itUsX3iIhvDnnHHT7WoCT76qI",
+    authDomain: "workservices-e4506.firebaseapp.com",
+    databaseURL: "https://workservices-e4506.firebaseio.com",
+    projectId: "workservices-e4506",
+    storageBucket: "workservices-e4506.appspot.com",
+    messagingSenderId: "232325006209",
+    appId: "1:232325006209:web:da0aca044337ef4b119e80",
+    measurementId: "G-K8BRW9Y8HL"
+  };
